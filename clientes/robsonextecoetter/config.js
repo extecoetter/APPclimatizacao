da6ee1c7b781a170f7bb02b17510245b57ebf2d5
extecoetter <extecoetterrobson@gmail.com>
2026-03-10 13:46:50 -0300

    Delete clientes/robsonextecoetter/config.js

--- clientes/robsonextecoetter/config.js
+++
@@ -1,8 +0,0 @@
-window.CLIENT_CONFIG = {
-  installer: {
-    nome: "Robson Andre Extecoetter",
-    whats: "(14) 99707-3447",
-    pix: "14 99707 3447 / Robson A Extecoetter"
-  },
-  logo: "/APPclimatizacao/clientes/robsonextecoetter/logo.png"
-};
