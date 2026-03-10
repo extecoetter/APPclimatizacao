28f3780f9f3de95bae4bf1313d070735a387c97b
extecoetter <extecoetterrobson@gmail.com>
2026-03-10 08:54:48 -0300

    Delete clientes/clientemodelo directory

--- clientes/clientemodelo/config.js
+++
@@ -1,9 +0,0 @@
-window.CLIENT_CONFIG = {
-  installer: {
-    nome: "Cliente Modelo Modelo",
-    whats: "(14) 99707-3447",
-    pix: "14 99707 3447 / Cliente Modelo"
-  },
-
-  logo: "/climatizaorcamentos/clientes/clientemodelo/logo.png"
-};
