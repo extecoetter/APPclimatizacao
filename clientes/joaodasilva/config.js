144e5750e7d03ba626b172efd8d2a8acd0be21a7
extecoetter <extecoetterrobson@gmail.com>
2026-03-11 09:28:55 -0300

    Add files via upload

---
+++ clientes/joaodasilva/config.js
@@ -0,0 +1,8 @@
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "Joao da Silva",
+    pix_nome: "Joao da Silva",
+    pix_chave: "11 98101 0005"
+  },
+  logo: "/APPclimatizacao/clientes/joaodasilva/logo.png"
+};
