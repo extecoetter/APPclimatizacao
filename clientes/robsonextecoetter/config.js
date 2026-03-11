58848e37ddf3acd3a29c05700bb3114e0b1965ea
extecoetter <extecoetterrobson@gmail.com>
2026-03-11 09:51:09 -0300

    Update config.js

--- clientes/robsonextecoetter/config.js
+++ clientes/robsonextecoetter/config.js
@@ -1,1 +1,8 @@
-window.CLIENT_CONFIG = { installer: { nome: "Robson Andre Extecoetter", whats: "(14) 99707-3447", pix: "14 99707 3447 / Robson A Extecoetter" }, logo: "/APPclimatizacao/clientes/robsonextecoetter/logo.png" };
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "Robson Andre Extecoetter",
+    whats: "(14) 99707-3447",
+    pix: "14 99707 3447 / Robson A Extecoetter"
+  },
+  logo: "/climatizaorcamentos/clientes/robsonextecoetter/logo.png"
+};
