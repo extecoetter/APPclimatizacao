e4d2130ea76e97a1e0640f75d0e0ffb2d39c8442
extecoetter <extecoetterrobson@gmail.com>
2026-03-12 08:44:02 -0300

    Add files via upload

---
+++ clientes/climaseven/config.js
@@ -0,0 +1,8 @@
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "ClimaSeven",
+    whats: "(67) 99848-1279",
+    pix: "57.201.385/0001-72"
+  },
+  logo: "/APPclimatizacao/clientes/climaseven/logo.png"
+};
