336447a7961f01eb4709e3b04a70937e34ea7053
extecoetter <extecoetterrobson@gmail.com>
2026-03-13 11:13:14 -0300

    Add files via upload

---
+++ clientes/testador/config.js
@@ -0,0 +1,8 @@
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "cliente teste",
+    whats: "99 9999 9999",
+    pix: "99 9999 9999"
+  },
+  logo: "/APPclimatizacao/clientes/testador/logo.png"
+};
