a92755661dd1a46eb7eed1d03ef8e86b01076152
extecoetter <extecoetterrobson@gmail.com>
2026-03-16 10:52:11 -0300

    Delete clientes/threfrigeracao directory

--- clientes/threfrigeracao/config.js
+++
@@ -1,8 +0,0 @@
-window.CLIENT_CONFIG = {
-  installer: {
-    nome: "TH Refrigeraçao",
-    whats: "(81) 98457-7976",
-    pix: "81 98457 7976"
-  },
-  logo: "/APPclimatizacao/clientes/threfrigeraçao/logo.png"
-};
