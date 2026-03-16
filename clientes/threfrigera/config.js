0c542331e0ba9c083148c7fe55f8d6442fb971a9
extecoetter <extecoetterrobson@gmail.com>
2026-03-16 10:54:04 -0300

    Add files via upload

---
+++ clientes/threfrigera/config.js
@@ -0,0 +1,8 @@
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "TH Refrigeração",
+    whats: "(81) 98457-7976",
+    pix: "81 98457 7976"
+  },
+  logo: "/APPclimatizacao/clientes/threfrigera/logo.png"
+};
