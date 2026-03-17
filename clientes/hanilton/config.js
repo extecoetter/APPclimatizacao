60beef73e0b4f166bcfe8e024c6c66f5a3ba9576
extecoetter <extecoetterrobson@gmail.com>
2026-03-17 17:14:58 -0300

    Add files via upload

---
+++ clientes/hanilton/config.js
@@ -0,0 +1,8 @@
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "Hanilton",
+    whats: "(24) 99922-0143",
+    pix: "24 99922 0143 / 301.663.497-04"
+  },
+  logo: "/APPclimatizacao/clientes/hanilton/logo.png"
+};
